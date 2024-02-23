d111fa4a90d809e069bc1d98b89b4f4365063b7e
Raphael Courouble <159543589+RaphiCourouble@users.noreply.github.com>
2024-02-23 14:21:08 +0100

    Mise à jour des nouveaux fichiers

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+# Importation des différents modules
+
+import pygame
+
+pygame.init()
+
 # Déclaration des variables
 
 screen_width = 1080
 screen_height = 720
+fps = 60
+logo = pygame.image.load("Paris_Survival_Games_Logo.png")
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 # Importation des différents modules
 
-import pygame
 from config import *
+import pygame
 
 pygame.init()
 
 # Création de l'écran principal
 
 pygame.display.set_caption("Paris Survival Games")
+pygame.display.set_icon(logo)
 main_screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Boucle du jeu
 
 running = True
+clock = pygame.time.Clock()
 
 while running:
+
+    clock.tick(fps)
 
     for event in pygame.event.get():
 
